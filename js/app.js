37dd7eb79347f373fba045c60aaa6076e0df3330
Chin Kong Lai <cklai26@yahoo.com>
2023-03-28 14:37:46 +0800

    Finished v1.0

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-js
+// Select elements
+const selectElement = selector => {
+    const element = document.querySelector(selector);
+    if(element) return element;
+    throw new Error(`Something went wrong, make sure that ${selector} exists`);
+};
+
+// Navbar elevation on scroll
+const scrollHeader = () => {
+    const headerElement = selectElement('#header');
+    if(this.scrollY >= 15){
+        headerElement.classList.add('activated');
+    }else{
+        headerElement.classList.remove('activated');
+    }
+};
+
+window.addEventListener('scroll', scrollHeader);
+
+// Open menu 
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+};
+
+menuToggleIcon.addEventListener('click', toggleMenu);
